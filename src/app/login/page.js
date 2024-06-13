cbbb9180e02a686b6abef1c538a3a6dbbddbbaaf
msyiham <muhammad.syiham.2105336@students.um.ac.id>
2024-06-13 14:43:40 +0700

    setup auth

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,43 +1,57 @@
-import React from "react";
+'use client';
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from 'next/navigation';
+import {FIREBASE_AUTH} from "../firebaseConfig";
 export default function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const router = useRouter(); // Initialize router
+    const auth = FIREBASE_AUTH
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            router.push('/'); // Redirect to home page or another protected page
+        } catch (error) {
+            console.error("Error logging in: ", error);
+            alert("Error logging in: " + error.message);
+        }
+    };
     return(
-        <section className="bg-home d-flex align-items-center" style={{backgroundImage:"url('/images/hero/bg3.jpg')", backgroundPosition:'center'}}>
+        <section className="bg-home d-flex align-items-center" style={{backgroundImage:"url('/images/bg3.jpeg')", backgroundPosition:'center'}}>
             <div className="bg-overlay bg-linear-gradient-2"></div>
             <div className="container">
                 <div className="row">
                     <div className="col-lg-4 col-md-5 col-12">
                         <div className="p-4 bg-white rounded shadow-md mx-auto w-100" style={{maxWidth:'400px'}}>
-                            <form>
+                            <form onSubmit={handleLogin}>
                                 <Link href="/"><Image src='/images/logo-dark.png' width={120} height={18} className="mb-4 d-block mx-auto" alt=""/></Link>
-                                <h6 className="mb-3 text-uppercase fw-semibold">Please sign in</h6>
                             
                                 <div className="mb-3">
-                                    <label className="form-label fw-semibold">Your Email</label>
-                                    <input name="email" id="email" type="email" className="form-control" placeholder="example@website.com"/>
+                                    <label className="form-label fw-semibold">Email</label>
+                                    <input name="email" id="email" type="email" className="form-control" placeholder="example@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                                 </div>
 
                                 <div className="mb-3">
                                     <label className="form-label fw-semibold" htmlFor="loginpass">Password</label>
-                                    <input type="password" className="form-control" id="loginpass" placeholder="Password"/>
+                                    <input type="password" className="form-control" id="loginpass" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
                                 </div>
                             
-                                <div className="d-flex justify-content-between">
+                                <div className="d-flex justify-content-center">
                                     <div className="mb-3">
                                         <div className="form-check">
-                                            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                            <label className="form-label form-check-label text-muted" htmlFor="flexCheckDefault">Remember me</label>
+                                            <span className="forgot-pass text-muted small mb-0"><Link href="/reset-password" className="text-muted">Lupa password ?</Link></span>
                                         </div>
                                     </div>
-                                    <span className="forgot-pass text-muted small mb-0"><Link href="/reset-password" className="text-muted">Forgot password ?</Link></span>
                                 </div>
                 
-                                <button className="btn btn-primary w-100" type="submit">Sign in</button>
+                                <button className="btn btn-primary w-100" type="submit">Masuk</button>
 
                                 <div className="col-12 text-center mt-3">
-                                    <span><span className="text-muted me-2 small">Dont have an account ?</span> <Link href="/signup" className="text-dark fw-semibold small">Sign Up</Link></span>
+                                    <span><span className="text-muted me-2 small">Tidak punya akun?</span> <Link href="/signup" className="text-dark fw-semibold small">Daftar</Link></span>
                                 </div>
                             </form>
                         </div>
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,43 +1,67 @@
-import React from "react";
+'use client';
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-
+import {FIREBASE_AUTH, FIRESTORE} from "../firebaseConfig";
+import {createUserWithEmailAndPassword} from "firebase/auth"
+import { doc, setDoc } from "firebase/firestore";
+import { useRouter } from 'next/navigation';
 export default function Signup(){
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const auth = FIREBASE_AUTH;
+    const db = FIRESTORE;
+    const router = useRouter();
+    const handleSignup = async (event) => {
+        event.preventDefault();
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            
+            // Save the user's name in Firestore
+            await setDoc(doc(db, "users", user.uid), {
+                name: name,
+                email: email
+            });
+            router.push('/login');
+            alert("Pendaftaran Berhasil!");
+
+        } catch (error) {
+            console.error("Error signing up: ", error);
+            alert("Error signing up: " + error.message);
+        }
+    };
     return(
-        <section className="bg-home d-flex align-items-center" style={{backgroundImage:"url('/images/hero/bg3.jpg')", backgroundPosition:'center'}}>
+        <section className="bg-home d-flex align-items-center" style={{backgroundImage:"url('/images/bg3.jpeg')", backgroundPosition:'center'}}>
             <div className="bg-overlay bg-linear-gradient-2"></div>
             <div className="container">
                 <div className="row">
                     <div className="col-lg-4 col-md-5 col-12">
                         <div className="p-4 bg-white rounded shadow-md mx-auto w-100" style={{maxWidth:'400px'}}>
-                            <form>
+                            <form onSubmit={handleSignup}>
                                 <Link href="/"><Image src="/images/logo-dark.png" width={120} height={18} className="mb-4 d-block mx-auto" alt=""/></Link>
-                                <h6 className="mb-3 text-uppercase fw-semibold">Register your account</h6>
+                                <h6 className="mb-3 fw-semibold">Daftarkan Akunmu</h6>
                             
                                 <div className="mb-3">
-                                    <label className="form-label fw-semibold">Your Name</label>
-                                    <input name="name" id="name" type="text" className="form-control" placeholder="Calvin Carlo"/>
+                                    <label className="form-label fw-semibold">Nama</label>
+                                    <input name="name" id="name" type="text" className="form-control" placeholder="Isilah Nama Lengkap" value={name} onChange={(e) => setName(e.target.value)} required/>
                                 </div>
 
                                 <div className="mb-3">
-                                    <label className="form-label fw-semibold">Your Email</label>
-                                    <input name="email" id="email" type="email" className="form-control" placeholder="example@website.com"/>
+                                    <label className="form-label fw-semibold">Email</label>
+                                    <input name="email" id="email" type="email" className="form-control" placeholder="example@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                                 </div>
 
                                 <div className="mb-3">
                                     <label className="form-label fw-semibold" htmlFor="loginpass">Password</label>
-                                    <input type="password" className="form-control" id="loginpass" placeholder="Password"/>
+                                    <input type="password" className="form-control" id="loginpass" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
                                 </div>
                             
-                                <div className="form-check mb-3">
-                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                    <label className="form-label form-check-label text-muted" htmlFor="flexCheckDefault">I Accept <Link href="#" className="text-primary">Terms And Condition</Link></label>
-                                </div>
-                
-                                <button className="btn btn-primary w-100" type="submit">Register</button>
+                                <button className="btn btn-primary w-100" type="submit">Daftar</button>
 
                                 <div className="col-12 text-center mt-3">
-                                    <span><span className="text-muted small me-2">Already have an account ? </span> <Link href="/login" className="text-dark fw-semibold small">Sign in</Link></span>
+                                    <span><span className="text-muted small me-2">Sudah punya akun? </span> <Link href="/login" className="text-dark fw-semibold small">Masuk</Link></span>
                                 </div>
                             </form>
                         </div>
