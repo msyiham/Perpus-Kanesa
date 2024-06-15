40ac5b5100d4ae3cf821daa1621311f81a35752c
msyiham <muhammad.syiham.2105336@students.um.ac.id>
2024-06-15 07:37:24 +0700

    cek login in page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,52 +1,66 @@
 'use client';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from 'next/navigation';
 import { FIREBASE_AUTH } from "../firebaseConfig";
+import Swal from 'sweetalert2';
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(null);
   const router = useRouter();
-  const searchParams = new URLSearchParams(window.location.search); // Get query parameters from the URL
-  const redirect = searchParams.get('redirect'); // Extract 'redirect' parameter from URL
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    setRedirect(searchParams.get('redirect'));
+  }, []);
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       await signInWithEmailAndPassword(FIREBASE_AUTH, email, password);
-      alert('Selamat! Anda berhasil masuk.');
-      if (redirect && redirect !== 'undefined') {
-        router.push(decodeURIComponent(redirect));
-      } else {
-        router.push('/');
-      }
+      Swal.fire({
+        title: 'Selamat!',
+        text: 'Anda berhasil masuk.',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      }).then(() => {
+        if (redirect && redirect !== 'undefined') {
+          router.push(decodeURIComponent(redirect));
+        } else {
+          router.push('/');
+        }
+      });
     } catch (error) {
       console.error('Login failed:', error);
-      alert('Error! Gagal Masuk. Tolong cek email dan password.');
+      Swal.fire({
+        title: 'Error!',
+        text: 'Gagal Masuk. Tolong cek email dan password',
+        icon: 'error',
+        confirmButtonText: 'Retry'
+      });
     }
   };
-  
-  
 
   return (
-    <section className="bg-home d-flex align-items-center" style={{backgroundImage:"url('/images/bg3.jpeg')", backgroundPosition:'center'}}>
+    <section className="bg-home d-flex align-items-center" style={{ backgroundImage: "url('/images/bg3.jpeg')", backgroundPosition: 'center' }}>
       <div className="bg-overlay bg-linear-gradient-2"></div>
       <div className="container">
         <div className="row">
           <div className="col-lg-4 col-md-5 col-12">
-            <div className="p-4 bg-white rounded shadow-md mx-auto w-100" style={{maxWidth:'400px'}}>
+            <div className="p-4 bg-white rounded shadow-md mx-auto w-100" style={{ maxWidth: '400px' }}>
               <form onSubmit={handleLogin}>
-                <Link href="/"><Image src='/images/logo-dark.png' width={120} height={18} className="mb-4 d-block mx-auto" alt=""/></Link>
+                <Link href="/"><Image src='/images/logo-dark.png' width={120} height={18} className="mb-4 d-block mx-auto" alt="Logo" /></Link>
                 <div className="mb-3">
                   <label className="form-label fw-semibold">Email</label>
-                  <input name="email" id="email" type="email" className="form-control" placeholder="example@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                  <input name="email" id="email" type="email" className="form-control" placeholder="example@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} required />
                 </div>
                 <div className="mb-3">
                   <label className="form-label fw-semibold" htmlFor="loginpass">Password</label>
-                  <input type="password" className="form-control" id="loginpass" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                  <input type="password" className="form-control" id="loginpass" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                 </div>
                 <div className="d-flex justify-content-center">
                   <div className="mb-3">
@@ -65,5 +79,5 @@ export default function Login() {
         </div>
       </div>
     </section>
-  )
+  );
 }
